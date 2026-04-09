ce881d66fa83401d7500d6bdb48641c81acd806d
Calin Teodor <t.calin@student.vu.nl>
2026-04-09 13:10:39 -0700

    Bump to v1.7.1, add pilot-updater binary

--- Formula/pilotprotocol.rb
+++ Formula/pilotprotocol.rb
@@ -1,26 +1,26 @@
 class Pilotprotocol < Formula
   desc "The network stack for AI agents - addresses, ports, tunnels, encryption, trust"
   homepage "https://pilotprotocol.network"
-  version "1.5.1"
+  version "1.7.1"
   license "AGPL-3.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/TeoSlayer/pilotprotocol/releases/download/v1.5.1/pilot-darwin-arm64.tar.gz"
-      sha256 "7f6bf6831cc7db4437a4b19e205ec26535940c0467199f2676eb2bc3475ca8fd"
+      url "https://github.com/TeoSlayer/pilotprotocol/releases/download/v1.7.1/pilot-darwin-arm64.tar.gz"
+      sha256 "5251decf57830ddc7cc08028a26ac2b163238946ec085e5705fb43ee91f03631"
     else
-      url "https://github.com/TeoSlayer/pilotprotocol/releases/download/v1.5.1/pilot-darwin-amd64.tar.gz"
-      sha256 "da61a4dd4cf7770bb9fc897757920cb8e0f07d38d74b01a02e51d87b2df149b7"
+      url "https://github.com/TeoSlayer/pilotprotocol/releases/download/v1.7.1/pilot-darwin-amd64.tar.gz"
+      sha256 "a62036f86cd377005903a9f5bc3be8a31223d36daaaad4170502399a98894531"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/TeoSlayer/pilotprotocol/releases/download/v1.5.1/pilot-linux-arm64.tar.gz"
-      sha256 "b37e8c01be6ace80aa8d906bc1e73d209cd56cbc82e379d3353f39ada16f22a4"
+      url "https://github.com/TeoSlayer/pilotprotocol/releases/download/v1.7.1/pilot-linux-arm64.tar.gz"
+      sha256 "18088d10886f7758c28990285a0d9dd37bd5bd2ce593c1a4132b1730cf1e5b22"
     else
-      url "https://github.com/TeoSlayer/pilotprotocol/releases/download/v1.5.1/pilot-linux-amd64.tar.gz"
-      sha256 "44217b4627f18524ee3d6227bdb8e0030d0476650cf98ecd4e50054cdaf98dc4"
+      url "https://github.com/TeoSlayer/pilotprotocol/releases/download/v1.7.1/pilot-linux-amd64.tar.gz"
+      sha256 "5e9181be0b3fda1bd1557be8041be84d668c3066024c94908c6a695c9972e9e0"
     end
   end
 
@@ -28,6 +28,7 @@ class Pilotprotocol < Formula
     bin.install "daemon" => "pilot-daemon"
     bin.install "pilotctl" => "pilotctl"
     bin.install "gateway" => "pilot-gateway"
+    bin.install "updater" => "pilot-updater"
   end
 
   def post_install
@@ -35,6 +36,11 @@ class Pilotprotocol < Formula
 
     config_dir = Pathname.new(Dir.home)/".pilot"
     config_dir.mkpath
+    (config_dir/"bin").mkpath
+
+    # Write version file for the auto-updater
+    version_file = config_dir/"bin/.pilot-version"
+    version_file.write "v#{version}\n"
 
     config_file = config_dir/"config.json"
     unless config_file.exist?
@@ -55,12 +61,12 @@ class Pilotprotocol < Formula
       Config written to ~/.pilot/config.json (if not already present).
 
       Get started:
-        pilotctl daemon start --hostname my-agent
+        pilotctl daemon start --hostname my-agent --email you@example.com
         pilotctl info
 
       Docs: https://pilotprotocol.network/docs
 
-      To start the daemon as a background service:
+      To start as background services:
         brew services start pilotprotocol
     EOS
   end
